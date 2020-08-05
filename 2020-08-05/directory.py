5c360673cdd95a5e8cd06391e6167f4a3e7e5595
nurse_mimi <mimzzy.co@gmail.com>
2020-08-05 23:55:56 +1400

    practice

--- 2020-08-05/directory.py
+++ 2020-08-05/directory.py
@@ -1,5 +1,7 @@
+# Unfinished. ran into some problems. WILL Research and come back to complete.
+
 from os import listdir
 from os.path import isfile, join
-files_list = [f for f in listdir('/home/2020-08-05') if isfile(join('/home/2020-08-05', f))]
+files_list = [f for f in listdir('/home/') if isfile(join('/home/', f))]
 
 print(files_list)
